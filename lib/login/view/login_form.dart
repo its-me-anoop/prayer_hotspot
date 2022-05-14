6574b6427330e156d75aae5b3887cf9c912f84d3
Anoop Jose <anoop@flutterly.co.uk>
2022-05-14 21:12:44 +0100

    Bug fixes

--- lib/login/view/login_form.dart
+++ lib/login/view/login_form.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:prayer_hotspot/login/login.dart';
 import 'package:prayer_hotspot/sign_up/sign_up.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+//import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:formz/formz.dart';
 
 class LoginForm extends StatelessWidget {
@@ -133,27 +133,27 @@ class _LoginButton extends StatelessWidget {
   }
 }
 
-class _GoogleLoginButton extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    return ElevatedButton.icon(
-      key: const Key('loginForm_googleLogin_raisedButton'),
-      label: const Text(
-        'SIGN IN WITH GOOGLE',
-        style: TextStyle(color: Colors.white),
-      ),
-      style: ElevatedButton.styleFrom(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(30),
-        ),
-        primary: theme.colorScheme.secondary,
-      ),
-      icon: const Icon(FontAwesomeIcons.google, color: Colors.white),
-      onPressed: () => context.read<LoginCubit>().logInWithGoogle(),
-    );
-  }
-}
+// class _GoogleLoginButton extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     final theme = Theme.of(context);
+//     return ElevatedButton.icon(
+//       key: const Key('loginForm_googleLogin_raisedButton'),
+//       label: const Text(
+//         'SIGN IN WITH GOOGLE',
+//         style: TextStyle(color: Colors.white),
+//       ),
+//       style: ElevatedButton.styleFrom(
+//         shape: RoundedRectangleBorder(
+//           borderRadius: BorderRadius.circular(30),
+//         ),
+//         primary: theme.colorScheme.secondary,
+//       ),
+//       icon: const Icon(FontAwesomeIcons.google, color: Colors.white),
+//       onPressed: () => context.read<LoginCubit>().logInWithGoogle(),
+//     );
+//   }
+// }
 
 class _SignUpButton extends StatelessWidget {
   @override
